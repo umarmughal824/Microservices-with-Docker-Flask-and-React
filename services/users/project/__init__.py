281c2b6e8f08e9bd1282ab8583158e4f82b73cc2
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2018-10-23 11:49:19 +0800

    Add config setting

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
