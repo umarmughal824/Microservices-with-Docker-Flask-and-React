13d82dbc8d6df3c93efba95add28f9ac25a45494
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2018-10-25 14:19:41 +0800

    Move config set from app hard code to docker-compse file

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,13 +1,17 @@
+import os
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
         'status': 'success',
-        'message': 'pong!'
+        'message': 'pong!',
+        'haha': 'hi there'
         })
