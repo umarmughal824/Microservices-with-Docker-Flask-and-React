09be0162edc513196739966e9f7aa3fb964c082e
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2018-10-27 15:10:26 +0800

    flask blueprint

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,8 +1,28 @@
+import unittest
+
 from flask.cli import FlaskGroup
 
-from project import app, db
+from project import create_app, db
+from project.api.models import User
 
-cli = FlaskGroup(app)
+app = create_app()
 
-if __name__ == '_main__':
+cli = FlaskGroup(create_app=create_app)
+
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command()
+def test():
+    """ Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('.', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+if __name__ == '__main__':
     cli()
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,64 +1,29 @@
 import os
 
-import click
-
-from flask import Flask, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask.cli import with_appcontext
-
-import unittest
-
-app = Flask(__name__)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
 
 # instantiate the db
-db = SQLAlchemy(app)  # new
+db = SQLAlchemy()
 
-# model
-class User(db.Model):
-    __tablename__ = 'users'
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # set up extensions
+    db.init_app(app)
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!',
-        'haha': 'hi there'
-        })
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-@click.command('recreate_db')
-@with_appcontext
-def recreate_db_init():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
 
-@click.command('test')
-@with_appcontext
-def test_init():
-    """ Runs the tests without code coverage"""
-    """ test path should be a directory that contains the tests you want to run, not the path to a single module. """
-    """ Try just using . as the directory (assuming you're running it from the top-level project/app directory) and see if that helps. """
-    tests = unittest.TestLoader().discover('.', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
-
-def init_app(app):
-    app.cli.add_command(recreate_db_init)
-    app.cli.add_command(test_init)
-
-init_app(app)
+    return app
