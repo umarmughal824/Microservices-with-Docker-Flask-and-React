4eae065e1deda9b0a8916719cfde83bb8de7b699
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2018-11-12 20:22:35 +0800

    add userstatus page

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -8,6 +8,7 @@ import About from './components/About';
 import NavBar from './components/NavBar';
 import Form from './components/Form';
 import Logout from './components/Logout';
+import UserStatus from './components/UserStatus';
 
 class App extends Component {
 
@@ -165,6 +166,7 @@ class App extends Component {
                     isAuthenticated={this.state.isAuthenticated}
                   />
                 )} />
+                <Route exact path='/status' component={UserStatus}/>
               </Switch>
             </div>
           </div>
