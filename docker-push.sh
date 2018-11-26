e1c377126d4145c40cbc20dd726d183606bf743c
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2018-11-26 15:29:29 +0800

    update ECR, part 5

--- docker-push.sh
+++ docker-push.sh
@@ -3,9 +3,9 @@
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
 
-  # new
   if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     export DOCKER_ENV=stage
+    export REACT_APP_USERS_SERVICE_URL="http://LOAD_BALANCER_STAGE_DNS_NAME"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
   fi
@@ -28,22 +28,22 @@ then
   then
 
     # users
-    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
+    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
     docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
     docker push $REPO/$USERS:$TAG
-    # users db
 
+    # users db
     docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
     docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
     docker push $REPO/$USERS_DB:$TAG
-    # client
 
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build- arg REACT_APP_USERS_SERVICE_URL=TBD # new
+    # client
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build- arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
-    # swagger
 
-    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
+    # swagger
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
     docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
     docker push $REPO/$SWAGGER:$TAG
   fi
